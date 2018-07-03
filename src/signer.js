4a642ac30d7d2dcea557ec8f114690cccd7761d8
Roman Zhukov <r.zhukov@it.ecommpay.com>
2018-07-03 19:59:18 +0300

    Initial commit

---
+++ src/signer.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const crypto = require('crypto');
+const converter = require('./converter');
+
+module.exports = (obj, salt) => {
+    const hmac = crypto.createHmac('sha512', salt);
+    hmac.update(converter(obj));
+    return hmac.digest('base64');
+};
