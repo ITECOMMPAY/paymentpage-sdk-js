3570680d9c023a5c594d7252245117e833e5e336
Roman Zhukov <zhukov.roman@gmail.com>
2018-12-03 15:20:04 +0300

    Fix callback handler

--- __tests__/data/callback.js
+++ __tests__/data/callback.js
@@ -1,11 +1,10 @@
 module.exports.secret = 'secret';
 
+// short callback body from Gate
 module.exports.data = {
-  body: {
-    payment: {
-      id: '000049',
-      status: 'success',
-    },
+  payment: {
+    id: '000049',
+    status: 'success',
   },
   signature: '/3TrhHVz9BHaLfgLyhoxO7dsQNzbzZa/TdO3pxQocXWwgVM6tGlzMLVfpOxFNTnkR9iiFVK/77ZedMcbkCjmng==',
 };
--- src/callback.js
+++ src/callback.js
@@ -6,10 +6,12 @@ const signer = require('./signer');
  */
 class Callback {
   constructor(secret, data) {
-    const obj = typeof data === 'string' ? JSON.parse(data) : data;
+    const obj = typeof data === 'string' ? JSON.parse(data) : Object.assign({}, data);
     this.secret = secret;
-    this.callback = obj.body;
+    this.callback = obj;
     this.signature = obj.signature;
+    // remove signature from callback before we check valid
+    delete this.callback.signature;
     if (!this.isValid()) {
       throw new Error('Invalid signature');
     }
